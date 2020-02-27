5660bc97eb4690dbbe8aa99ed1f713b76aea48cd
JVP-cmd <61412434+JVP-cmd@users.noreply.github.com>
2020-02-28 00:47:05 +0200

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,14 +1,89 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication1;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
-/**
- *
- * @author joshv
- */
+
+
 public class Client {
-    
+
+	public static int numClients = 0;
+	private String ipAddress;
+	private String clientName;
+
+
+	public Client(String ipAddress, String clientName){
+		numClients++;
+		this.ipAddress = ipAddress;
+		this.clientName = clientName;
+	}
+
+
+	public String getIpAddress(){
+		return ipAddress;
+	}
+
+	public String getClientName(){
+		return clientName;
+	}
+                public void uploadFile(String filename, Socket s){
+            while(true){
+                try{
+ DataInputStream din=new DataInputStream(s.getInputStream());
+            DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+            
+            String Data;
+            Data=din.readUTF();
+            if(!Data.equals("stop")){
+            System.out.println("Uploading File: "+filename);
+            dout.writeUTF(filename);
+            dout.flush();
+            File file=new File(filename);
+            FileInputStream fileIn=new FileInputStream(file);
+            long sz=(int)file.length();
+            byte b[]=new byte[1024];
+            int read;
+            dout.writeUTF(Long.toString(sz));
+            dout.flush();
+            while((read=fileIn.read(b))!=1){
+            dout.write(b,0,read);
+            dout.flush();}
+            fileIn.close();
+            dout.flush();}
+            dout.writeUTF("stop");
+            dout.flush();
+             din.close();
+            //s.close();
+            }catch(Exception e){System.out.println(e);}
+           
+            
+            }}
+                public void DownloadtoClient( Socket s){
+                    try{
+                DataInputStream din=new DataInputStream(s.getInputStream());
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String str=""; String filename="";
+                    while(!str.equals("stop")){
+                    str=br.readLine();
+                    dout.writeUTF(str);
+                    dout.flush();
+                    filename=din.readUTF();
+                    long sz=Long.parseLong(din.readUTF());
+                     byte b[]=new byte [1024];
+                    FileOutputStream fos=new FileOutputStream(new File(filename),true);
+                    long bytesRead;
+                    do{
+                    bytesRead=din.read(b,0,b.length);
+                    fos.write(b,0,b.length);}
+                    while(!(bytesRead<1024));{
+                     fos.close();
+                     dout.close();
+                     s.close();}}}
+                    catch(Exception e){System.out.println();}
+}
 }
--- ServerMain.java
+++ ServerMain.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication1;
+import java.net.*;
+import java.io.*;
 
-/**
- *
- * @author joshv
- */
+
 public class ServerMain {
-    
+	public static void main(String[] args) { // Server main thread [child threads will be made in server object]
+		try {
+			String ip = InetAddress.getLocalHost().getHostAddress();
+			String fileLoc = "res";
+			Server server = new Server(ip, fileLoc, 59090);
+			server.initialize();
+
+			while(true){
+				server.listen();
+				System.out.println("Connection received");
+			}
+		}
+		catch(Exception e){
+			System.out.println("An error occurred while running the server: " + e.getMessage());
+			System.exit(1);
+		}
+	}
 }
