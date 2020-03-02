8f59e506df1ed26bf6c9075fe7eb89e7d57f18ca
mzamo65 <54236714+mzamo65@users.noreply.github.com>
2020-03-02 12:51:33 +0200

    Add files via upload

--- Server.java
+++ Server.java
@@ -16,39 +16,77 @@ class Server{
     public static void main(String args[])throws Exception{
 
         Scanner sc=new Scanner(System.in);
+        boolean excess = false;
+        boolean success = false;
+        String userName= "";
 
         System.out.println("Welcome to the server");
+        System.out.printf("User name: ");
+
+        while(!success) {               // Checks if username is in the records and repeats the prompt if it's incorrect
+            userName = sc.nextLine();
+            success = users(userName);
+            if(!success) System.out.println("Unknown user, try again.");
+        }
+
+        System.out.println("Excess level? (Public/Admin): ");
+        success = false;
+        String ex = sc.nextLine();
+        if(ex.equals("Public")){
+            excess = false;
+        }
+
+        else if(ex.equals("Admin")){
+            System.out.printf("Password: ");
+
+            while(!success){
+                String pass = sc.nextLine();
+                success  = password(userName, pass);
+                if(!success) System.out.println("Try again");
+            }
+            excess = true;
+            System.out.println("Welcome back "+userName+"!!");
+        }
+
+
         System.out.println("Select an action: ");
         System.out.println("<View> - To view files on server");
         System.out.println("<Download> - To download a file");
-        System.out.println("<Upload> - To upload a file");
-        System.out.println("<Permission> Set Permission");
+        if(excess)      System.out.println("<P> - To set Permissions");
+
 
         String request = sc.nextLine();
-        if(request.equals("View")){view();}
 
-        else if(request.equals("Perm")){
+        if(request.equals("View")){
+            if(excess) {
+                view("./Server/".concat(userName)+"/Private");
+                view("./Server/".concat(userName));
+            }
+            else view("./Server/".concat(userName));
+        }
+
+        else if(request.equals("P") && excess){
             System.out.println("Type filename:");
             String filename=sc.nextLine();
-            System.out.println("Make readable? (Y/N)");
+            System.out.println("Make Private? (Y/N)");
             String p = sc.nextLine();
-
-            if(p.equals("N"))   perm(filename, false);
-            System.out.println("Make private? (Y/N)");
-            if(sc.nextLine().equals("Y")){
-                perm(filename);
-            }
+            File f = new File("./Server/".concat(userName), filename);
+            perm(f, userName, filename);
         }
 
         else if (request.equals("Download")){
             System.out.println("Give the filename");
             String filename=sc.nextLine();
-            download(filename);}
+            download(filename, userName);}
 
     }
 
-    public static void view(){
-        Path path = Paths.get("/home/j/jlxkhu003/Documents/Networks/Server");
+    /**
+     * View contents in the server
+     */
+
+    public static void view(String folderPath){
+        Path path = Paths.get(folderPath);
 
         try(Stream<Path> subPaths = Files.walk(path,1)){
 
@@ -62,37 +100,74 @@ class Server{
         }
     }
 
-    public static void perm(String file, boolean bool){
-      File f = new File(file);
-      if(f.exists()){
-          if(f.setReadable(bool)){
-              System.out.println("Permissions Changed");
-          }
-      }
-      else{
-          System.out.println("File does not exist");
-      }
+    /**
+     * Checks if password is correct for the given username
+     * @param username
+     * @param password
+     * @return
+     * @throws FileNotFoundException
+     */
+    private static boolean password(String username, String password) throws FileNotFoundException {
+        boolean bool = false;
+        File file = new File("userInfo.txt");
+        Scanner scan = new Scanner(file);
+        while(scan.hasNext()){
+            if(scan.next().equals(username) && scan.next().equals(password)){
+                bool = true;
+            }
+        }
+        return bool;
     }
 
-    public static void perm(String file){
-        File f = new File("/home/j/jlxkhu003/Documents/Networks");
-        File makePrivate = new File("/home/j/jlxkhu003/Documents/Networks/Server/Private");
+
+    /**
+     * Checks if given user name is known to the server
+     * @param username
+     * @return
+     * @throws FileNotFoundException
+     */
+    public static boolean users(String username) throws FileNotFoundException {
+        boolean bool = false;
+        File file = new File("userInfo.txt");
+        Scanner scan = new Scanner(file);
+        while(scan.hasNext()){
+            if(scan.next().equals(username)){
+                bool = true;
+            }
+            scan.next();
+        }
+        return bool;
+    }
+
+
+    /**
+     * Makes file private or invisible to users using public access
+     * @param ofile
+     * @param username
+     * @param fname
+     */
+    public static void perm(File ofile, String username, String fname){
+        File makePrivate = new File("./Server/".concat(username)+"/Private/" + fname);
         try{
-            Files.copy(f.toPath(), makePrivate.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(ofile.toPath(), makePrivate.toPath(), StandardCopyOption.REPLACE_EXISTING);
         }catch(IOException e){}
-        System.out.println("File is now private");
+        //ofile.delete();
+        System.out.println("Moved to private folder");
     }
 
-
-
-    public static void download(String filename) throws IOException {
+    /**
+     * Allows client to download a file from server
+     * @param filename
+     * @throws IOException
+     */
+    public static void download(String filename, String username) throws IOException {
 
         while(true)
         {
             //create server socket on port 5000
             ServerSocket ss=new ServerSocket(5000);
             System.out.println ("Waiting for request");
-            Socket s=ss.accept();
+            Socket s = ss.accept();
             System.out.println ("Connected With "+s.getInetAddress().toString());
             DataInputStream din=new DataInputStream(s.getInputStream());
             DataOutputStream dout=new DataOutputStream(s.getOutputStream());
@@ -108,7 +183,7 @@ class Server{
                     dout.writeUTF(filename);
                     dout.flush();
 
-                    File f=new File(filename);
+                    File f=new File("./Server/".concat(username), filename);
                     FileInputStream fin=new FileInputStream(f);
                     long sz=(int) f.length();
 
